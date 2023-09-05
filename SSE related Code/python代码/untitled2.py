9427c4474132861761e7dd90f725df72807a80cd
2431828726 <110757584+2431828726@users.noreply.github.com>
2023-09-05 18:46:26 +0800

    time serie.py

--- SSE related Code/python代码/untitled2.py
+++ SSE related Code/python代码/untitled2.py
@@ -1,9 +1,10 @@
+#py绘制时间序列
 import os
 import matplotlib.pyplot as plt
 
 folder_path = r'E:\研究生阶段文件\test'
 files = ["80.txt", "100.txt", "120.txt", "140.txt",  "180.txt"]
-colors = ['b', 'g', 'r', 'c', 'y']  # Different colors for different files
+colors = ['b', 'g', 'r', 'c', 'y']  
 
 for i, file in enumerate(files):
     file_path = os.path.join(folder_path, file)
@@ -15,8 +16,8 @@ for i, file in enumerate(files):
         for line in f:
             data = line.split()
             if len(data) >= 4:
-                x.append(float(data[2]))  # Assuming the year is in the third column
-                y.append(float(data[3]))  # Assuming the value is in the fourth column
+                x.append(float(data[2]))  
+                y.append(float(data[3])) 
     
     plt.plot(x, y, color=colors[i], label=file)
 
